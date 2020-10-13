3e08cbb9d543a41c6231e6e535571a0340a775db
JG HAN <gojkhan13@gmail.com>
2020-10-13 23:39:50 +0900

    10.13 주석변경2

--- src/kr/co/softcampus/controller/HomeController.java
+++ src/kr/co/softcampus/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-	//test22
+	//test33
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
 		return "index";
