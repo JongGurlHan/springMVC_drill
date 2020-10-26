30757f0977b4c4378d2ea0a4cd97a8ec509958ca
JG HAN <60098769+JongGurlHan@users.noreply.github.com>
2020-10-26 16:31:45 +0900

    10.26 예외처리

---
+++ HandleException/src/com/springdrill/controller/TestController2.java
@@ -0,0 +1,28 @@
+package com.springdrill.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController2 {
+	
+	@GetMapping("/test2")
+	public String test1() {
+
+		ArrayList<String> list = null;
+		list.add("문자열");
+		
+		return "test2";
+	}
+
+//	@ExceptionHandler(java.lang.NullPointerException.class)
+//	public String exception2() {
+//		return "error2";
+//	}
+	
+	
+	
+}
